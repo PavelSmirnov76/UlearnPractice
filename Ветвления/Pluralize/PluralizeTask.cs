9d0c088766f62514704064d46fe0322c92e276b7
PavelSmirnov76 <56821290+PavelSmirnov76@users.noreply.github.com>
2021-05-25 12:46:45 +0300

    Add files via upload

---
+++ Ветвления/Pluralize/PluralizeTask.cs
@@ -0,0 +1,15 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+			if (count % 10 == 1 && count % 100 != 11)
+				return "рубль";
+			if (count % 10 > 1 && count % 10 < 5 && (count % 100 < 10 || count % 100 > 20))
+				return "рубля";
+			return "рублей";
+		}
+	}
+}
