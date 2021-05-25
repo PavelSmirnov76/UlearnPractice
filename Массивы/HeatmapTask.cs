c8697ace875f35097adf962dcdc2ddfd34766a6a
PavelSmirnov76 <56821290+PavelSmirnov76@users.noreply.github.com>
2021-05-25 12:45:35 +0300

    Add files via upload

---
+++ Массивы/HeatmapTask.cs
@@ -0,0 +1,31 @@
+﻿namespace Names
+{
+    internal static class HeatmapTask
+    {
+        private const int DayInMonth = 30;
+        private const int MonthInYear = 12;
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var xLabelsDay = new string[DayInMonth];
+            var yLabelsMonth = new string[MonthInYear];
+            var masResult = new double[DayInMonth, MonthInYear];
+            
+            for (int i = 0; i < DayInMonth; i++)
+                xLabelsDay[i] = $"{i+2}";
+            for (int i = 0; i < MonthInYear; i++)
+                yLabelsMonth[i] = $"{i + 1}";
+
+            foreach (var name in names)
+                if (name.BirthDate.Day != 1)
+                    masResult[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                masResult,
+                xLabelsDay,
+                yLabelsMonth
+              
+                );
+        }
+    }
+}
---
+++ Массивы/HistogramTask.cs
@@ -0,0 +1,24 @@
+﻿namespace Names
+{
+    internal static class HistogramTask
+    {
+        private const int DayInMonth = 31;
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var xLabelsDay = new string[DayInMonth];
+            var yCountPeople = new double[DayInMonth];
+
+            for ( int i = 0; i < DayInMonth; i ++)
+                xLabelsDay[i] = $"{i+1}";
+            
+            foreach(var item in names)
+                if (item.Name == name && item.BirthDate.Day != 1)
+                    yCountPeople[item.BirthDate.Day - 1]++;
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                xLabelsDay,
+                yCountPeople);
+        }
+    }
+}
